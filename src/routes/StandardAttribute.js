fd39351747dc4d69dc8976fd79b3ecb2e0e1233e
obstechlab@gmail.com <obstechlab@gmail.com>
2020-11-11 19:25:11 -0300

    Answering with status 200 json success to the online data capture service

--- src/routes/StandardAttribute.js
+++ src/routes/StandardAttribute.js
@@ -27,89 +27,95 @@ Output: Void (stores the data in the db)
 Description: Calls the b-Games-ApirestPostAtt service 
 */
 router.post('/StandardAttributes/', (req,res,next)=>{
-    var post_data = req.body;
-    if(!req.body.id_player || !req.body.id_player|| !req.body.nameat|| !req.body.namecategory|| !req.body.data|| !req.body.data_type|| !req.body.input_source|| !req.body.date_time){
-        return res.sendStatus(400).json({
-            error: 'Missing data'
-          })
-    }
-    console.log(post_data);
-    var id_player = Number(post_data.id_player);
-    var nameat = post_data.nameat;
-    var namecategory = post_data.namecategory;
-    var dat = Number(post_data.data);
-    var data_type = post_data.data_type;
-    var input_source = post_data.input_source;
-    var date_time = post_data.date_time;
 
+    try {
+        var post_data = req.body;
+        if(!req.body.id_player || !req.body.id_player|| !req.body.nameat|| !req.body.namecategory|| !req.body.data|| !req.body.data_type|| !req.body.input_source|| !req.body.date_time){
+            return res.sendStatus(400).json({
+                error: 'Missing data'
+            })
+        }
+        console.log(post_data);
+        var id_player = Number(post_data.id_player);
+        var nameat = post_data.nameat;
+        var namecategory = post_data.namecategory;
+        var dat = Number(post_data.data);
+        var data_type = post_data.data_type;
+        var input_source = post_data.input_source;
+        var date_time = post_data.date_time;
+
+        
+        const data2 = JSON.stringify({
+            id_player: id_player,
+            nameat:nameat,
+            namecategory:namecategory,
+            data:dat,
+            data_type:data_type,
+            input_source:input_source,
+            date_time:date_time
+        })
+
+        console.log(data2);
+
+        var options = {
+            host : 'bgames-apirestpostatt.herokuapp.com',
+            path: ('/attributes/'),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(data2),
+            }
+        };
+
+        var data = 
+        {
+                id_player: Number(post_data.id_player),
+                nameat: post_data.nameat,
+                namecategory: post_data.namecategory,
+                dat: Number(post_data.data),
+                data_type: post_data.data_type,
+                input_source: post_data.input_source,
+                date_time:post_data.date_time
+        };
+
+        var url = "http://"+options.host + options.path;
+        console.log("URL "+url);
+        // construct the URL to post to a publication
+        const MEDIUM_POST_URL = url;
+
+        const response = fetch(MEDIUM_POST_URL, {
+            method: "post",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json",
+                "Accept-Charset": "utf-8"
+                },
+                body: JSON.stringify({
+                    id_player: id_player,
+                    nameat:nameat,
+                    namecategory:namecategory,
+                    data:dat,
+                    data_type:data_type,
+                    input_source:input_source,
+                    date_time:date_time
+                })
+        })
+        .then(res => res.status(200).json('Success'))
+        .then(json => console.log("Response of API: "+json));
+
+        const messageData = response;
+
+        // the API frequently returns 201
+        if ((response.status !== 200) && (response.status !== 201)) {
+            console.error(`Invalid response status ${ response.status }.`);
+            throw messageData;
+        }else{
+
+        }
+    } catch (error) {
+        next(error)
+    }
     
-    const data2 = JSON.stringify({
-        id_player: id_player,
-        nameat:nameat,
-        namecategory:namecategory,
-        data:dat,
-        data_type:data_type,
-        input_source:input_source,
-        date_time:date_time
-      })
-
-    console.log(data2);
-
-    var options = {
-        host : 'bgames-apirestpostatt.herokuapp.com',
-        path: ('/attributes/'),
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(data2),
-          }
-    };
-
-    var data = 
-    {
-            id_player: Number(post_data.id_player),
-            nameat: post_data.nameat,
-            namecategory: post_data.namecategory,
-            dat: Number(post_data.data),
-            data_type: post_data.data_type,
-            input_source: post_data.input_source,
-            date_time:post_data.date_time
-    };
-
-    var url = "http://"+options.host + options.path;
-    console.log("URL "+url);
-    // construct the URL to post to a publication
-    const MEDIUM_POST_URL = url;
-
-    const response = fetch(MEDIUM_POST_URL, {
-        method: "post",
-        headers: {
-            "Content-type": "application/json",
-            "Accept": "application/json",
-            "Accept-Charset": "utf-8"
-            },
-            body: JSON.stringify({
-                id_player: id_player,
-                nameat:nameat,
-                namecategory:namecategory,
-                data:dat,
-                data_type:data_type,
-                input_source:input_source,
-                date_time:date_time
-              })
-    })
-    .then(res => res.json())
-    .then(json => console.log("Response of API: "+json));
-
-    const messageData = response;
-
-    // the API frequently returns 201
-    if ((response.status !== 200) && (response.status !== 201)) {
-        console.error(`Invalid response status ${ response.status }.`);
-        throw messageData;
-    }else{
-
-    }
 
 })
 
