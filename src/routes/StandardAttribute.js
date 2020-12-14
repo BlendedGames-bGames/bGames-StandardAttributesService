de6b75b26374dc83c80aaf3176b769f66cd0d949
Tisks <leiser.mahu@usach.com>
2020-12-13 22:37:18 -0300

    Hotfix conversions

--- src/routes/StandardAttribute.js
+++ src/routes/StandardAttribute.js
@@ -80,7 +80,7 @@ router.post('/standard_attributes_apis', jsonParser, wrap(async(req,res,next) =>
     var adquired_subattributes ={  
         "id_player": id_player,        
         "id_sensor_endpoint": id_sensor_endpoint,
-        "id_conversion": id_conversions,   
+        "id_conversions": id_conversions,   
         "id_subattributes":id_subattributes,
         "new_data": results
     }
